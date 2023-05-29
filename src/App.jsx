4a0e8a47217c19c4b1619a5db797a1783f957240
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-05-29 15:36:32 -0300

    2 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import extendedTheme from './types/styled.d';
 import { RouterPages } from './Routes';
 import { BrowserRouter } from 'react-router-dom';
 
-
 function App() {
 
   return (
@@ -14,7 +13,7 @@ function App() {
       <BrowserRouter>
         <RouterPages/>
       </BrowserRouter>
-      
+
     </ThemeProvider>
   );
 }
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import { CompleteOrderPage } from './pages/CompleteOrder';
 import { HomePage } from './pages/Home/index';
-
+ 
 export const RouterPages = () => {
   return (
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/completeOrder" element={<CompleteOrderPage />} />
       </Routes>
   );
 };
