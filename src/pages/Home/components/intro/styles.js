a8f97c6c2ed5547d7a9ac787a94e740636e71bcb
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-06-02 14:30:42 -0300

    2 files modified

--- src/pages/Home/components/intro/styles.js
+++ src/pages/Home/components/intro/styles.js
@@ -4,5 +4,12 @@ export const IntroContainer = styled.section`
   background: black;
   width: 100%;
   height: 34rem;
-  
+`
+
+//Tanto contém o conteudo da esquerda quanto da direita, ou seja, englobe tudo
+export const IntroContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.5rem;
 `
